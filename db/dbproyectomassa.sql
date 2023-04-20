83255680e9f9058595b24e4db996d0d2e7a4805e
matiasgcg05 <matiasgastoncordobagil@gmail.com>
2023-04-19 21:17:21 -0300

    #6 resubido DB.

--- db/dbproyectomassa.sql
+++ db/dbproyectomassa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-04-2023 a las 02:01:02
+-- Tiempo de generación: 20-04-2023 a las 02:15:16
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
