77542384898535ddd0bb76f518216d209a1d0b08
matiasgcg05 <matiasgastoncordobagil@gmail.com>
2023-04-26 19:09:57 -0300

    #8 descargue plugins

---
+++ wp-content/plugins/betterlinks/assets/js/betterlinks-gutenberg.core.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b88755d2cf7492ff4a1c');
---
+++ wp-content/plugins/betterlinks/assets/js/betterlinks.core.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '703a825c55c6a7042058');
